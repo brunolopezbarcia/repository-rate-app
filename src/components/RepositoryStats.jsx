5a77ceda1dda5ee238c1decb3780cf4f362cd42c
brunolopezbarcia <brunolopar46@gmail.com>
2022-05-15 07:32:41 -0700

    Better Code 🙂

--- src/components/RepositoryStats.jsx
+++ src/components/RepositoryStats.jsx
@@ -1,33 +1,47 @@
-import react from "react";
+import React from "react";
 import { View, Text, FlatList } from "react-native";
 import StyledText from "./StyledText";
 
-const parseThousands = value => {
-    return value >= 1000
-    ? `${Math.round(value / 100) / 10}k`
-    : String(value)
-}
+const parseThousands = (value) => {
+  return value >= 1000 ? `${Math.round(value / 100) / 10}k` : String(value);
+};
 const RepositoryStats = (props) => {
   return (
     <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
       <View>
-        <StyledText align='center' fontWeight='bold'>Forks:</StyledText>
-        <StyledText align='center'>{parseThousands(props.forksCount)}</StyledText>
+        <StyledText align="center" fontWeight="bold">
+          Forks:
+        </StyledText>
+        <StyledText align="center">
+          {parseThousands(props.forksCount)}
+        </StyledText>
       </View>
       <View>
-        <StyledText align='center' fontWeight='bold'>Stars:</StyledText>
-        <StyledText align='center'>{parseThousands(props.stargazersCount)}</StyledText>
+        <StyledText align="center" fontWeight="bold">
+          Stars:
+        </StyledText>
+        <StyledText align="center">
+          {parseThousands(props.stargazersCount)}
+        </StyledText>
       </View>
       <View>
-        <StyledText align='center' fontWeight='bold'>Rating:</StyledText>
-        <StyledText align='center'>{parseThousands(props.ratingAverage)}</StyledText>
+        <StyledText align="center" fontWeight="bold">
+          Rating:
+        </StyledText>
+        <StyledText align="center">
+          {parseThousands(props.ratingAverage)}
+        </StyledText>
       </View>
       <View>
-        <StyledText align='center' fontWeight='bold'>ReviewCount:</StyledText>
-        <StyledText align='center'>{parseThousands(props.reviewCount)}</StyledText>
+        <StyledText align="center" fontWeight="bold">
+          ReviewCount:
+        </StyledText>
+        <StyledText align="center">
+          {parseThousands(props.reviewCount)}
+        </StyledText>
       </View>
     </View>
   );
-}
+};
 
-export default RepositoryStats
+export default RepositoryStats;
--- src/components/StyledText.jsx
+++ src/components/StyledText.jsx
@@ -1,48 +1,48 @@
-import react from "react";
-import { Text, StyleSheet } from "react-native";
-import theme from "../theme.js";
+import react from 'react'
+import { Text, StyleSheet } from 'react-native'
+import theme from '../theme.js'
 
 const styles = StyleSheet.create({
-    text:{
-        fontSize: theme.fontSizes.body,
-        color: theme.colors.textPrimary,
-        fontFamily: theme.fonts.main,
-        fontWeight: theme.fontWeights.normal
-    },
-    bold: {
-        fontWeight: theme.fontWeights.bold,
-    },
-    blue: {
-        color: '#09f',
-    },
-    subheading: {
-        fontSize: theme.fontSizes.subheading,
-    },
-    colorPrimary: {
-        color: theme.colors.textprimary,
-    },
-    colorSecondary: {
-        color: theme.colors.textsecondary,
-    },
-    align: {
-    textAlign: 'center',
-    }
+  text: {
+    fontSize: theme.fontSizes.body,
+    color: theme.colors.textPrimary,
+    fontFamily: theme.fonts.main,
+    fontWeight: theme.fontWeights.normal
+  },
+  bold: {
+    fontWeight: theme.fontWeights.bold
+  },
+  blue: {
+    color: '#09f'
+  },
+  subheading: {
+    fontSize: theme.fontSizes.subheading
+  },
+  colorPrimary: {
+    color: theme.colors.textprimary
+  },
+  colorSecondary: {
+    color: theme.colors.textsecondary
+  },
+  align: {
+    textAlign: 'center'
+  }
 })
 
-export default function StyledText({children, color, fontSize, fontWeight, style, align, ... restOfProps}) {
-    const textStyle= [
-        styles.text,
-        color === 'primary' && styles.colorPrimary,
-        color === 'secondary' && styles.colorSecondary,
-        fontSize === 'subheading' && styles.subheading,
-        fontWeight === 'bold' && styles.bold,
-        align === 'center' && styles.align,
-        style
-    ]
+export default function StyledText ({ children, color, fontSize, fontWeight, style, align, ...restOfProps }) {
+  const textStyle = [
+    styles.text,
+    color === 'primary' && styles.colorPrimary,
+    color === 'secondary' && styles.colorSecondary,
+    fontSize === 'subheading' && styles.subheading,
+    fontWeight === 'bold' && styles.bold,
+    align === 'center' && styles.align,
+    style
+  ]
 
-    return (
+  return (
         <Text style={textStyle} {... restOfProps}>
             {children}
         </Text>
-    )
+  )
 }
